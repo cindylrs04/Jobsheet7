7b293647ab3bb50f5b08594d9d686c40c1c71403
cindylrs04 <larasaticindy231@gmail.com>
2023-10-09 14:00:17 +0700

    ForVar208

--- WhileGaji08.java
+++ WhileGaji08.java
@@ -3,6 +3,7 @@ public class WhileGaji08 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int jumlahKaryawan;
-
+        double gajiLembur = 0, totalGajiLembur = 0;
+        
         }
 }
