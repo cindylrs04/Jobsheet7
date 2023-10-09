53abfa73c1edcdf935bd98d6753f3d35a5bed038
cindylrs04 <larasaticindy231@gmail.com>
2023-10-09 12:34:53 +0700

    For08

---
+++ For08.java
@@ -0,0 +1,7 @@
+public class For08 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hasil kuadrat dari " + 1 + " adalah " + (i * i));
+
+        }    }
+}
