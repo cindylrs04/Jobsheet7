399aeefe9e1e3a3bc62e57302687764d6934da25
cindylrs04 <larasaticindy231@gmail.com>
2023-10-09 14:26:55 +0700

    DoWhile08

---
+++ DoWhile08.java
@@ -0,0 +1,18 @@
+public class DoWhile08 {
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            System.out.println(x);
+        } while (++x <= 8);
+        System.out.println("Program berhenti");
+
+        int X = 10;
+        do {
+            System.out.println(x);
+        } while (++x <= 8);
+        System.out.println("Program berhenti");
+    } 
+    
+}
+
+
